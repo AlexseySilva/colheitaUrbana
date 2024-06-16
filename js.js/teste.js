9058ff6ca44c9da6e1cfc9a76dd20748c9055007
AlexseySilva <alexsoadsk8@hotmail.com>
2024-06-16 14:55:12 -0300

    novasMudancas

---
+++ js.js/teste.js
@@ -0,0 +1,40 @@
+
+function limparCamposEndereco() {
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+function preencherCamposEndereco(dados) {
+    document.getElementById('rua').value = dados.logradouro;
+    document.getElementById('bairro').value = dados.bairro;
+    document.getElementById('cidade').value = dados.localidade;
+    document.getElementById('estado').value = dados.uf;
+}
+
+async function consultarCEP() {
+    const cep = document.getElementById('cep').value.replace(/\D/g, ''); // Remove caracteres não numéricos do CEP
+
+    if (cep.length !== 8) {
+        alert('CEP inválido. Favor inserir um CEP válido.');
+        limparCamposEndereco();
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+
+        if (data.erro) {
+            alert('CEP não encontrado. Favor verificar o CEP informado.');
+            limparCamposEndereco();
+        } else {
+            preencherCamposEndereco(data);
+        }
+    } catch (error) {
+        console.error('Erro ao consultar API de CEP:', error);
+        alert('Erro ao consultar API de CEP. Tente novamente mais tarde.');
+        limparCamposEndereco();
+    }
+}
